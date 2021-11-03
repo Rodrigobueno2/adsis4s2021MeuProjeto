cbc63263f392816525cbeeb625f5c62be83cdcda
Rodrigobueno2 <73552177+Rodrigobueno2@users.noreply.github.com>
2021-11-03 00:26:34 -0300

    Adicionando mais métodos de requisição http no controller e jogando suas regras de negócio no service, além da criação de duas classes exception dessa regra de negócio

--- Projeto/src/main/java/com/unicesumar/adsis4s/Projeto/carro/CarroController.java
+++ Projeto/src/main/java/com/unicesumar/adsis4s/Projeto/carro/CarroController.java
@@ -3,11 +3,18 @@ package com.unicesumar.adsis4s.Projeto.carro;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.unicesumar.adsis4s.Projeto.base.IdentificadoresDiferentesAoAtualizar;
 
 @RestController
 @RequestMapping("/api/carros")
@@ -21,7 +28,26 @@ public class CarroController {
    }
    
    @PostMapping
+   @ResponseStatus(code = HttpStatus.CREATED)
    public String salvarCarro(@RequestBody Carro novo) {
 	   return service.salvarCarro(novo).getId();
+   }
+   
+   @PutMapping("/{id}")
+   public void atualizarCarro(@PathVariable("id") String id, @RequestBody Carro carroAtualizado){
+	   if(!id.equals(carroAtualizado.getId())) {
+		   throw new IdentificadoresDiferentesAoAtualizar(); 
+	   }
+	   service.atualizarCarro(carroAtualizado);
+   }
+   
+   @DeleteMapping("/{id}")
+   public void deletarCarro(@PathVariable("id") String id) {
+	   service.deletarCarro(id);
+   }
+   
+   @GetMapping("/{id}")
+   public Carro obterPorId(@PathVariable("id") String id) {
+	   return service.pesquisarCarro(id);
    }
 }
--- Projeto/src/main/java/com/unicesumar/adsis4s/Projeto/carro/CarroService.java
+++ Projeto/src/main/java/com/unicesumar/adsis4s/Projeto/carro/CarroService.java
@@ -2,6 +2,7 @@ package com.unicesumar.adsis4s.Projeto.carro;
 
 import java.util.List;
 import com.unicesumar.adsis4s.Projeto.base.RegistroJaExistente;
+import com.unicesumar.adsis4s.Projeto.base.RegistroNaoExiste;
 
 import javax.transaction.Transactional;
 
@@ -19,12 +20,36 @@ public class CarroService {
 	  return repo.findAll();
    }
 
-public Carro salvarCarro(Carro novo) {
-	if(repo.findById(novo.getId()).isPresent()) {
-		throw new RegistroJaExistente();
+	public Carro salvarCarro(Carro novo) {
+		if(repo.findById(novo.getId()).isPresent()) {
+			throw new RegistroJaExistente();
+		}
+		return repo.save(novo);
 	}
-	return repo.save(novo);
-}
+	
+	public void atualizarCarro(Carro carroAtualizar) {
+		if(repo.findById(carroAtualizar.getId()).isEmpty()) {
+			throw new RegistroNaoExiste();
+		}
+		
+		repo.save(carroAtualizar);
+	}
+	
+	public void deletarCarro(String id) {
+		if(repo.findById(id).isEmpty()) {
+			throw new RegistroNaoExiste();
+		}
+		
+		repo.deleteById(id);
+	}
+	
+	public Carro pesquisarCarro(String id) {
+		if(repo.findById(id).isEmpty()) {
+			throw new RegistroNaoExiste();
+		}
+		
+		return repo.findById(id).get();
+	}
    
    
 }
